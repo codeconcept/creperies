5f61852a6515f25536feb1fbab5e64f5fb122ba4
codeconcept <contact@codeconcept.fr>
2020-12-15 11:34:43 +0100

    Cleaning

--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {
-  AngularFirestore,
-  DocumentChangeAction,
-} from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
